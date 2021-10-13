7c8ed83bfc5cc92e2c2a8c45df8baf46757cb48c
Moros <71281300+Moros0741@users.noreply.github.com>
2021-10-13 03:20:42 -0400

    commit

--- modules/helpers.js
+++ modules/helpers.js
@@ -92,7 +92,6 @@ exports.getCompliance = function (memberProfile) {
 exports.getSeconds = function (duration) {
     time = duration.splice(0, duration.length - 1)
     let index = duration.splice(duration.length - 1, duration.length)
-    console.log(time, index)
     time_convert = {"s":1, "m":60, "h":3600,"d":86400}
     newtime= Number(time) * time_convert[index]
     return newtime
